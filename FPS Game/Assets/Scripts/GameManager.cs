7b722ccdb2adc7194161519f5c9cbe377ba210d4
NBacon659 <104604606+NBacon659@users.noreply.github.com>
2023-04-09 10:38:30 -0700

    Added game manager
    Added the game manager, hooked up the proper menus in the editor.

---
+++ FPS Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [Header("----- Player Information -----")]
+    public GameObject player;
+    public PlayerController playerScript;
+
+    [Header("----- UI Information -----")]
+    public GameObject activeMenu;
+    public GameObject pauseMenu;
+    public GameObject winMenu;
+    public GameObject loseMenu;
+
+    public bool isPaused;
+    public int enemiesRemaining;
+
+    float TIME_SCALE_DEFAULT;
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+        TIME_SCALE_DEFAULT = Time.timeScale;
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && activeMenu == null)
+        {
+            isPaused = !isPaused;
+            activeMenu = pauseMenu;
+            activeMenu.SetActive(isPaused);
+
+            if (isPaused)
+            {
+                PauseState();
+            }
+            else
+            {
+                ResumeState();
+            }
+        }
+    }
+
+    public void PauseState()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void ResumeState()
+    {
+        Time.timeScale = TIME_SCALE_DEFAULT;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
+
+    public void UpdateGameGoal(int amount)
+    {
+        enemiesRemaining += amount;
+
+        if (enemiesRemaining <= 0)
+        {
+            activeMenu = winMenu;
+            activeMenu.SetActive(true);
+            PauseState();
+        }
+    }
+}
---
+++ FPS Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //TODO: Setup Player Controller Implementation
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
