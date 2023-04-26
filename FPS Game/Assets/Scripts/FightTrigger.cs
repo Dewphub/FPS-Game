26d5e3525fb6afd2c5f49553aa5f65e8a2393cea
Dewphub <98754253+Dewphub@users.noreply.github.com>
2023-04-26 16:08:45 -0400

    Arena trigger code

--- FPS Game/Assets/Scripts/FightTrigger.cs
+++ FPS Game/Assets/Scripts/FightTrigger.cs
@@ -4,56 +4,19 @@ using UnityEngine;
 
 public class FightTrigger: MonoBehaviour
 {
-    [SerializeField] Renderer model;
-    [SerializeField] GameObject trigger;
-    [SerializeField] int intervalTime;
-    [SerializeField] Transform[] spawnPos;
-    [SerializeField] int prefabMaxNum;
-
-    public List<GameObject> prefabList = new List<GameObject>();
-
-    int prefabsSpawnCount;
-    bool playerInRange;
-    bool isSpawning;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //GameManager.Instance.UpdateGameGoal(prefabMaxNum);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (playerInRange && !isSpawning && prefabsSpawnCount < prefabMaxNum)
-        {
-            StartCoroutine(Spawn());
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = true;
-        }
-    }
-
-    IEnumerator Spawn()
-    {
-        isSpawning = true;
-        GameObject prefabClone = Instantiate(trigger, spawnPos[Random.Range(0, spawnPos.Length)].position, trigger.transform.rotation);
-        prefabList.Add(prefabClone);
-        prefabsSpawnCount++;
-        yield return new WaitForSeconds(intervalTime);
-        isSpawning = false;
-    }
+    [SerializeField] Transform[] block;
+    [SerializeField] Vector3 triggerPos;
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerInRange = false;
+        if(other.CompareTag("Player"))
+        { 
+
+            foreach (Transform t in block)
+            {
+                t.position = triggerPos;
+             }    
         }
     }
+
 }
