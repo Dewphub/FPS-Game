c0e567a0d59656c6dc67ea83bec5a3f0e7d7ffa9
Riley Darling <evilunsc@gmail.com>
2023-05-09 04:25:44 -0600

    Attempted Boss Fight, It failed horribly
    Added a boss and gave it a state machine using the animator, however a sudden bug arose with the trigger of it's animations.

---
+++ FPS Game/Assets/Scripts/Boss Scripts/Boss.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public int attackDamage = 20;
+
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
+
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider[] colInfo = Physics.OverlapSphere(pos, attackRange, attackMask);
+        foreach (Collider collider in colInfo)
+        {
+            collider.GetComponent<PlayerController>().TakeDamage(attackDamage);
+        }
+    }
+}
