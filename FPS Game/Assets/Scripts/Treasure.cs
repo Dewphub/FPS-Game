fde28589ad628c94f5d1ec43a209b33a503d5475
Dewphub <98754253+Dewphub@users.noreply.github.com>
2023-05-10 04:45:42 -0400

    Player prefab update and Blockout progress; added object pickup

--- FPS Game/Assets/Scripts/Treasure.cs
+++ FPS Game/Assets/Scripts/Treasure.cs
@@ -10,13 +10,11 @@ public class Treasure : MonoBehaviour
 
     private Animator animator;
       
-    // Start is called before the first frame update
     void Start()
     {
        animator = GetComponent<Animator>(); 
     }
 
-    // Update is called once per frame
     void Update()
     {
     }
