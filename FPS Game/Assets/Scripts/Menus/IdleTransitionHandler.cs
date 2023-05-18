ecb76f366e0afdec4295809bfe44bc8a61ab41b0
Riley Darling <evilunsc@gmail.com>
2023-05-18 13:32:44 -0600

    Fixed animations for main menu screen
    Fixed animations for main menu screen

--- FPS Game/Assets/Scripts/Menus/IdleTransitionHandler.cs
+++ FPS Game/Assets/Scripts/Menus/IdleTransitionHandler.cs
@@ -12,54 +12,16 @@ public class IdleTransitionHandler : MonoBehaviour
     {
         StartCoroutine(ChangeState());
     }
-    public void ChangeAnimationState(int stateNum)
-    {
-        switch(stateNum)
-        {
-            case 0:
-                anim.SetInteger("IdleState", 0);
-                if(!isTransitioning)
-                {
-                    StartCoroutine(ChangeState());
-                }
-                break;
-            case 1:
-                anim.SetInteger("IdleState", 1);
-                if (!isTransitioning)
-                {
-                    StartCoroutine(ChangeState());
-                }
-                break;
-            case 2:
-                anim.SetInteger("IdleState", 2);
-                if (!isTransitioning)
-                {
-                    StartCoroutine(ChangeState());
-                }
-                break;
-            case 3:
-                anim.SetInteger("IdleState", 3);
-                if (!isTransitioning)
-                {
-                    StartCoroutine(ChangeState());
-                }
-                break;
-            default:
-                anim.SetInteger("IdleState", -1);
-                if (!isTransitioning)
-                {
-                    StartCoroutine(ChangeState());
-                }
-                break;
-        }
-    }
 
     IEnumerator ChangeState()
     {
-        isTransitioning = true;
-        int animationPhase = Random.Range(0, 4);
-        ChangeAnimationState(animationPhase);
-        yield return new WaitForSeconds(transitionDelayTime);
-        isTransitioning = true;
+        while (true)
+        {
+            int lastState = anim.GetInteger("IdleState");
+            while (anim.GetInteger("IdleState") == lastState)
+                anim.SetInteger("IdleState", Random.Range(0, 4));
+            yield return new WaitForSeconds(transitionDelayTime);
+            anim.SetTrigger("IdleFinished");
+        }
     }
 }
